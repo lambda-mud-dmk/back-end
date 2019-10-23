17f7172fa85cac810c09b98b3f234f10e555f537
David Quiñones <daquinons@users.noreply.github.com>
2019-10-23 15:07:49 +0200

    Remove CORS

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -25,8 +25,9 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
+CORS_ORIGIN_ALLOW_ALL = DEBUG
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
